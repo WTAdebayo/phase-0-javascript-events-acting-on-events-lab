ecb69432cecc70358eaf0c6f3e9dc0c6bb3190b3
Adewole Adebayo <wole1105@gmail.com>
2022-06-09 03:21:59 -0700

    commit tree 04c8d9883b9725b6d3821ca611cc4c10270cc2bb

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+document.addEventListener('keydown', event => {
+    if (event.key === 'ArrowLeft') {
+   moveDodgerLeft();
+}
+ } )
+ 
+ const moveDodgerLeft = () => {
+ const leftNumbers = dodger.style.left.replace("px", "");
+ const left = parseInt(leftNumbers, 10);
+ 
+ if (left > 0) {
+ dodger.style.left = `${left-1}px`;
+ }
+}
+
+const moveDodgerRight = () => {
+    const leftNumbers = dodger.style.left.replace('px', "" );
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left+1}px`
+    }
+}
+
+document.addEventListener('keydown' , event => {
+    if (event.key === 'ArrowRight') {
+    moveDodgerRight();
+    }
+})
